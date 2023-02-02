ba691fa3b655342786ac57cc17b929e1a1e8d9cb
SadickYahaya <52420004+SYahaya-Abubakar@users.noreply.github.com>
2023-02-02 11:37:17 +0000

    header carousel

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,7 +1,48 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import Projects from '../views/Projects.vue'
+import Contact from '../views/Contact.vue'
+import Departments from '../views/Departments.vue'
+import Media from '../views/Media.vue'
+import Documents from '../views/Documents.vue'
 
 const routes = [
-  
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/projects',
+    name: 'Projects',
+    component: Projects
+  },
+  {
+    path: '/documents',
+    name: 'Documents',
+    component: Documents
+  },
+  {
+    path: '/departments',
+    name: 'Departments',
+    component: Departments
+  },
+  {
+    path: '/media',
+    name: 'Media',
+    component: Media
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: Contact
+  },
 ]
 
 const router = createRouter({
