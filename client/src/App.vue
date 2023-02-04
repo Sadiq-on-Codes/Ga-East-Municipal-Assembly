e958a44a8e62836162022c7944d509d7edd26392
SadickYahaya <52420004+SYahaya-Abubakar@users.noreply.github.com>
2023-02-04 18:36:04 +0000

    navbar done

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,10 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <Navbar />
 </template>
+
+<script setup lang="ts">
+import Navbar from '@/components/Navbar'
+</script>
 
 <style>
 #app {
@@ -15,16 +15,7 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  font-family: 'Inter', sans-serif;
 }
 </style>
---
+++ client/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    require('flowbite/plugin')
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      'button-bg': '#25C200',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
