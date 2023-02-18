48befe3464c02b5dc8aa33362dac5f06411caaaa
SadickYahaya <52420004+SYahaya-Abubakar@users.noreply.github.com>
2023-02-18 17:22:25 +0000

    dark mode

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,8 +1,8 @@
 <template>
-  <nav class="bg-white h-auto border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+  <nav class="bg-white h-auto border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900">
     <div class="container flex flex-wrap items-center justify-between mx-auto">
       <a href="#" class="flex items-center">
-        <img src="../assets/ga-east-logo.jpg" class="h-auto w-14 mr-3" alt="Ga East Logo" />
+        <img src="../assets/logo-transparent.png" class="h-auto w-14 mr-3" alt="Ga East Logo" />
         <span
           class="lg-logo self-center text-2xl tracking-wide font-bold whitespace-nowrap dark:text-white text-logo-color uppercase text-left">Ga
           East <br />
@@ -12,7 +12,7 @@
       </a>
       <div class="flex md:order-2">
         <a href="https://gema.gov.gh:2096/" target="_blank"><button type="button"
-            class="button text-white uppercase font-semibold bg-button-bg focus:ring-4 focus:outline-none focus:ring-blue-300 text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 hover:bg-button-bg-hover">
+            class="button text-white uppercase font-semibold bg-button-bg focus:ring-4 focus:outline-none focus:ring-green-300 text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-button-bg dark:hover:bg-button-bg-hover dark:focus:bg-button-bg-hover hover:bg-button-bg-hover">
             Staff Mail
           </button></a>
         <button data-collapse-toggle="navbar-cta" type="button"
