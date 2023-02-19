c1cdb46b460c7bb3b8389af8044dc573e4ff46c7
SadickYahaya <52420004+SYahaya-Abubakar@users.noreply.github.com>
2023-02-19 18:31:09 +0000

    refactoring

---
+++ client/src/components/Events.vue
@@ -0,0 +1,119 @@
+<template>
+    <section class="lg:w-3/4 md:w-3/4 mt-20 mx-auto sm:px-6 lg:px-4">
+      <div
+        class="text-sm font-medium text-center text-gray-500 border-b border-gray-300 dark:text-gray-400 dark:border-gray-700"
+      >
+        <ul class="flex text-center flex-wrap justify-between">
+          <li class="mr-2">
+            <a
+              href="#"
+              class="inline-block p-4 text-button-bg-hover border-b-2 border-transparent active rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+              >Upcoming Events</a
+            >
+          </li>
+          <li class="mr-2">
+            <a
+              href="#"
+              class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:border-gray-300 dark:text-blue-500 dark:border-blue-500"
+              aria-current="page"
+              >Past Events</a
+            >
+          </li>
+          <li class="mr-2">
+            <a
+              href="#"
+              class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+              >Notice</a
+            >
+          </li>
+        </ul>
+      </div>
+      <div class="grid grid-cols-2 mt-10 gap-5 h-auto">
+          <div class="flex flex-col items-start gap-5">
+            <img class="event-image-large" src="../assets/event-3.jpeg" alt="" />
+            <span
+              class="text-white uppercase font-semibold bg-button-bg focus:ring-4 focus:outline-none focus:ring-green-300 text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-button-bg dark:hover:bg-button-bg-hover dark:focus:bg-button-bg-hover"
+              ><img
+                class="inline-block mr-1.5"
+                src="../assets/tag.svg"
+                alt=""
+              />
+              UP NEXT</span
+            >
+            <span
+              class="w-3/4 text-xl capitalize font-semibold text-left text-gray-900 dark:text-white"
+              >2021 POPULATION AND HOUSING CENSUS, Starting on the 7th of July,
+              2022
+            </span>
+            <span class="text-gray-400">March 6th 2023</span>
+          </div>
+          <div>
+            <div
+              class="flex gap-10 justify-between pb-4 border-b border-gray-200"
+            >
+              <div class="flex flex-col items-start gap-5 w-3/5">
+                <span
+                  class="text-xl capitalize font-semibold text-left text-gray-900 dark:text-white"
+                  >2021 POPULATION AND HOUSING CENSUS, Starting on the 7th of
+                  July, 2022
+                </span>
+                <span class="text-gray-400">March 6th 2023</span>
+              </div>
+              <div>
+                <img
+                  class="event-image-small h-auto"
+                  src="../assets/event-5.jpeg"
+                  alt=""
+                />
+              </div>
+            </div>
+            <div
+              class="flex gap-10 justify-between mt-10 pb-4 border-b border-gray-200"
+            >
+              <div class="flex flex-col items-start gap-5 w-3/5">
+                <span
+                  class="text-xl capitalize font-semibold text-left text-gray-900 dark:text-white"
+                  >2021 POPULATION AND HOUSING CENSUS, Starting on the 7th of
+                  July, 2022
+                </span>
+                <span class="text-gray-400">March 6th 2023</span>
+              </div>
+              <div>
+                <img
+                  class="event-image-small h-auto"
+                  src="../assets/event-5.jpeg"
+                  alt=""
+                />
+              </div>
+            </div>
+            <div
+              class="flex gap-10 justify-between mt-10 mb-20 pb-4 border-b border-gray-200"
+            >
+              <div class="flex flex-col items-start gap-5 w-3/5">
+                <span
+                  class="text-xl capitalize font-semibold text-left text-gray-900 dark:text-white"
+                  >2021 POPULATION AND HOUSING CENSUS, Starting on the 7th of
+                  July, 2022
+                </span>
+                <span class="text-gray-400">March 6th 2023</span>
+              </div>
+              <div>
+                <img
+                  class="event-image-small h-auto"
+                  src="../assets/event-5.jpeg"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+    </section>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
