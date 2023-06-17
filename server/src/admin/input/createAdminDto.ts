97fe9a8ffc2edfa6732747af62245326148a09c2
JudeEdemNyahe <58336301+CrimsonMckay@users.noreply.github.com>
2023-06-17 20:02:35 +0000

    eslint rules and admin login functionalities

--- server/src/admin/input/createAdminDto.ts
+++ server/src/admin/input/createAdminDto.ts
@@ -1,12 +1,18 @@
-import { IsEmail, Length } from 'class-validator';
+import { IsEmail, Length, IsNotEmpty } from 'class-validator';
 
 export class CreateAdminDto {
   @Length(3)
+  @IsNotEmpty({ message: 'Username is a required field and cannot be empty' })
   username: string;
+
   @IsEmail()
+  @IsNotEmpty({ message: 'Email is a required field and cannot be empty' })
   email: string;
+
   @Length(8)
+  @IsNotEmpty({ message: 'Password is a required field and cannot be empty' })
   password: string;
+
   @Length(8)
   retypedPassword: string;
 }
