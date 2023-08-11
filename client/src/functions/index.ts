13cb4116dc3861dbb126a4906299d63e005e41a7
Sadick Abubakar Yahaya <sadickashton@gmail.com>
2023-08-11 11:30:15 +0000

    Update index.ts

--- client/src/functions/index.ts
+++ client/src/functions/index.ts
@@ -19,7 +19,7 @@ export const scrollToTop = () => {
   }
 
   export const appendBaseURL = (imageURL: string): string => {
-    if (!imageURL?.startsWith('https://gema.gov.gh')) {
+    if (!imageURL?.startsWith('https://gema.gov.gh/images/')) {
       return `https://gema-gh-6b3bf99ee2e9.herokuapp.com/uploads/${imageURL}`;
     } 
     return imageURL;
