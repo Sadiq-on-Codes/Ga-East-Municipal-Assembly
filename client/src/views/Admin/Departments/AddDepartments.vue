b41e437a4769ac8100e3daf0bbc8a89c69479274
Sadick Abubakar Yahaya <52420004+SYahaya-Abubakar@users.noreply.github.com>
2023-09-10 17:53:51 +0000

    admin units section

--- client/src/views/Admin/Departments/AddDepartments.vue
+++ client/src/views/Admin/Departments/AddDepartments.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="flex gap-10 flex-col max-w-5xl mx-auto justify-center mt-28 ml-[30%]">
-    <h1 class="text-xl uppercase font-semibold text-[#322121] dark:text-white">{{ isEditing ? "Edit Department" : "Add Department" }}</h1>
+    <h1 v-if="isUnit" class="text-xl uppercase font-semibold text-[#322121] dark:text-white">{{ isEditing ? "Edit Unit" : "Add Unit" }}</h1>
+    <h1 v-else class="text-xl uppercase font-semibold text-[#322121] dark:text-white">{{ isEditing ? "Edit Department" : "Add Department" }}</h1>
+
+    <div class="text-left mt-20">
+      <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Is this a unit?</label>
+      <input type="checkbox" id="title" @checked="handleChecked" v-model="isUnit"
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-fit p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder="Enter post title" required/>
+    </div>
+
+
     <div class="text-left">
       <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
       <input type="text" id="title"
         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        placeholder="Enter post title" required v-model="data.title" />
+        :placeholder="isUnit ? 'Enter unit title' : 'Enter department title'" required v-model="data.title" />
     </div>
 
     <div class="text-left h-96 dark:text-white">
@@ -14,7 +24,22 @@
       <QuillEditor contentType="html" theme="snow" v-model:content="data.description" />
     </div>
 
-    <button type="button" @click="handleAddDepartment"
+    <div v-if="isUnit" class="text-left mt-20 dark:text-white">
+        <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select
+          category</label>
+        <select placeholder="Select category" id="countries" v-model="data.department"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+          <option disabled>Select Category</option>
+          <option :value="department.id" v-for="department in allDepartments" :key="department.id">{{ department.name }}</option>
+          
+        </select>
+      </div>
+
+    <button v-if="isUnit" type="button" @click="handleAddUnit"
+      class="mt-20 self-center w-fit mb-20 button text-white font-semibold bg-button-bg focus:ring-4 focus:outline-none focus:ring-green-300 text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-button-bg dark:hover:bg-button-bg-hover dark:focus:bg-button-bg-hover hover:bg-button-bg-hover">
+      {{ isEditing ? "Update Unit" : "Add Unit" }}
+    </button>
+    <button v-else type="button" @click="handleAddDepartment"
       class="mt-20 self-center w-fit mb-20 button text-white font-semibold bg-button-bg focus:ring-4 focus:outline-none focus:ring-green-300 text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-button-bg dark:hover:bg-button-bg-hover dark:focus:bg-button-bg-hover hover:bg-button-bg-hover">
       {{ isEditing ? "Update Department" : "Add Department" }}
     </button>
@@ -37,19 +62,25 @@ let successMessage = ref('');
 let showSuccessMessage = ref(false);
 let errorAlert = ref(false);
 let errorMessage = ref('');
+let isUnit = ref(false)
 const isEditing = ref(false);
 const route = useRoute();
 const postId = computed(() => route.params.id);
 const router = useRouter();
 const data = reactive({
   title: '',
-  description: ''
+  description: '',
+  department: ''
 })
 
 onMounted(() => {
   initTooltips();
   getDepartmentDetails();
 });
+
+const handleChecked = () => {
+  isUnit.value = !isUnit.value;
+}
 
 const departmentInfo = ref([]);
 const getDepartmentDetails = async () => {
@@ -62,7 +93,7 @@ const getDepartmentDetails = async () => {
         departmentInfo.value = documentData
         data.title = documentData.name;
         data.description = documentData.about;
-      } catch (error) {
+      } catch (error: any) {
         errorAlert.value = true;
         errorMessage.value = error.message
       }
@@ -71,6 +102,34 @@ const getDepartmentDetails = async () => {
     }
   }
 }
+
+const handleAddUnit = async () => {
+  try {
+    if (!isEditing.value) {
+      await axios.post(`${url}/unit`, { name: data.title, about: data.description, departmentId: data.department })
+    } else {
+      await axios.patch(`${url}/unit/update/${postId.value}`, { name: data.title, about: data.description, departmentId: data.department })
+    }
+    showSuccessMessage.value = true
+    successMessage = response.data.message;
+    setTimeout(() => {
+      showSuccessMessage.value = false;
+
+      if (isEditing.value) {
+        router.push('/admin/view-departments');
+      } else {
+        window.location.href = "/add-department"
+      }
+    }, 1000);
+  }
+  catch (error: any) {
+    errorAlert.value = true;
+    errorMessage.value = error.message;
+    setTimeout(() => {
+      errorAlert.value = false;
+    }, 5000)
+  }
+};
 
 const handleAddDepartment = async () => {
   try {
@@ -91,7 +150,7 @@ const handleAddDepartment = async () => {
       }
     }, 1000);
   }
-  catch (error) {
+  catch (error: any) {
     errorAlert.value = true;
     errorMessage.value = error.message;
     setTimeout(() => {
@@ -99,6 +158,16 @@ const handleAddDepartment = async () => {
     }, 5000)
   }
 };
+
+const allDepartments: any = ref([]);
+axios.get(`${url}/departments`)
+  .then((response: any) => {
+    allDepartments.value = response.data;
+    console.error(allDepartments.value);
+  })
+  .catch((error: string) => {
+    console.error(error);
+  });
 
 </script>
 <style>
