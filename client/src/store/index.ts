7c8b83f24004551e308b4abd1b0d5c843a7eee69
Sadick Abubakar Yahaya <52420004+SYahaya-Abubakar@users.noreply.github.com>
2023-09-14 20:47:39 +0000

    login functionality

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,39 +1,64 @@
+import { url } from '@/functions/endpoint';
 import router from '@/router';
-import { createStore } from 'vuex'
-import createPersistedState from 'vuex-persistedstate'
-import { email, password } from '../functions/endpoint'
+import axios from 'axios';
+import { createStore } from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
+
+
+function fetchAuthToken(username: string, password: string) {
+  return new Promise((resolve, reject) => {
+    axios.post(`${url}/admin/auth/login`, {
+      username: username,
+      password: password,
+    })
+    .then((response) => {
+      resolve(response.data);
+    })
+    .catch((error) => {
+      reject(error); 
+    });
+  });
+}
 
 interface RootState {
   isLoggedIn: boolean;
-  email: string | null;
-  loginEmail: string;
-  loginPassword: string;
+  token: string | null;
+  username: string | null;
+  successMessage: string;
 }
 
 const store = createStore<RootState>({
   state: {
     isLoggedIn: false,
-    email: null,
-    loginEmail: email,
-    loginPassword: password
+    token: null,
+    username: null,
+    successMessage: "",
   },
   mutations: {
-    login(state, email) {
+    login(state, { token, username }) {
       state.isLoggedIn = true;
-      state.email = email;
+      state.token = token;
+      state.username = username;
     },
     logout(state) {
       state.isLoggedIn = false;
+      state.token = null;
     },
   },
   actions: {
-    login({ commit }, { email, password }: { email: string, password: string }) {
-      if (email == this.state.loginEmail && password == this.state.loginPassword) {
-        commit('login', email);
-        router.push('/admin/dashboard');
-      } else {
-        throw new Error('Invalid credentials');
-      }
+    login({ commit }, { username, password }: { username: string, password: string }) {
+      fetchAuthToken(username, password)
+        .then((tokenData) => {
+          const { token }: any = tokenData;
+          commit('login', { token, username });
+          this.state.successMessage = "Login Successful"
+          setTimeout(() => {
+            router.push('/admin/dashboard');
+          }, 2000)
+        })
+        .catch((error) => {
+          throw new Error('Invalid credentials');
+        });
     },
     logout({ commit }) {
       commit('logout');
@@ -42,12 +67,12 @@ const store = createStore<RootState>({
   },
   getters: {
     isLoggedIn: (state) => state.isLoggedIn,
-    email: (state) => state.email,
+    email: (state) => state.username,
   },
   plugins: [
     createPersistedState({
       key: 'my-app',
-      paths: ['isLoggedIn'],
+      paths: ['isLoggedIn', 'token'],
     }),
   ],
 });
