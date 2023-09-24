356e562cca38fa9507188876740376c0aecf210c
Sadick Abubakar Yahaya <52420004+SYahaya-Abubakar@users.noreply.github.com>
2023-09-24 21:53:35 +0000

    reusable components

---
+++ client/src/components/Inputs/SelectField.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="text-left" :class="className">
+      <label :for="id" class="text-left block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ label }}</label>
+      <select
+        :id="id"
+        :class="[defaultSelectClasses, customSelectClasses]"
+        :placeholder="placeholder"
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)"
+      >
+        <option disabled>{{ placeholder }}</option>
+        <option :value="option.id" v-for="option in options">{{ option[param] }}</option>
+      </select>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import { computed } from 'vue';
+  
+  // Props
+  const {
+    label,
+    id,
+    placeholder = 'Select an option',
+    modelValue,
+    selectClasses: customSelectClasses, // Custom select classes prop
+    options,
+    optionValue = 'id',
+  } = defineProps<{
+    label: string;
+    id: string;
+    placeholder?: string;
+    modelValue: any;
+    param: string;
+    selectClasses?: string; // Custom select classes prop
+    options: Array<{ id: any; text: string }>;
+    optionValue?: string;
+  }>(); 
+  
+  // Default select classes
+  const defaultSelectClasses = computed(() => {
+    const classes = [
+      'bg-gray-50',
+      'border',
+      'border-gray-300',
+      'text-gray-900',
+      'text-sm',
+      'rounded-lg',
+      'focus:ring-blue-500',
+      'focus:border-blue-500',
+      'block',
+      'w-full',
+      'p-2.5',
+      'dark:bg-gray-700',
+      'dark:border-gray-600',
+      'dark:placeholder-gray-400',
+      'dark:text-white',
+      'dark:focus:ring-blue-500',
+      'dark:focus:border-blue-500',
+    ];
+  
+    return classes.join(' ');
+  });
+  </script>
+  
--- client/src/views/Admin/Slider/AddSlider.vue
+++ client/src/views/Admin/Slider/AddSlider.vue
@@ -1,34 +1,19 @@
 <template>
   <div class="flex gap-10 flex-col max-w-5xl mx-auto justify-center mt-28 ml-[30%]">
     <h1 class="text-xl uppercase font-semibold text-[#322121] dark:text-white">Add Slider</h1>
-    <div class="text-left">
-      <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
-      <input type="text" id="title" v-model="slider.title"
-        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        placeholder="Enter slider title" required />
-    </div>
 
-    <div class="text-left">
-      <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Short
-        description</label>
-      <input type="text" id="description" v-model="slider.description"
-        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        placeholder="Enter slider description" required />
-    </div>
+    <InputField label="Title" id="title" type="text" placeholder="Enter slider title" :isRequired="true"
+      v-model="slider.title" />
 
-    <div class="mx-auto w-full">
-      <label class="block mb-2 text-left text-sm font-medium text-gray-900 dark:text-white" for="user_avatar">Upload
-        slider image</label>
-      <input @change="handleImageChange"
-        class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-        aria-describedby="user_avatar_help" id="user_avatar" type="file" />
-      <div class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="user_avatar_help"></div>
-    </div>
+    <InputField label="Short description" id="description" type="text" placeholder="Enter slider description" :isRequired="false"
+      v-model="slider.description" />
 
-    <button type="button" @click="handleAddSlider"
-      class="mt-10 self-center w-40 mb-20 button text-white font-semibold bg-button-bg focus:ring-4 focus:outline-none focus:ring-green-300 text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-button-bg dark:hover:bg-button-bg-hover dark:focus:bg-button-bg-hover hover:bg-button-bg-hover">
-      Add Slider
-    </button>
+    <InputField  label="Upload slider image" id="description" type="file" placeholder="Enter slider description" :isRequired="false"
+       @change="handleImageChange" inputClasses="'block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400'" />
+
+    <Button :buttonText="'Add Slider'" :isDisabled="slider.title === '' || !slider.image || uploading"
+      :uploading="uploading" :handleClick="handleAddSlider" />
+
   </div>
   <SuccessMessage :showSuccessMessage="showSuccessMessage" :successMessage="successMessage" />
   <ErrorMessage :errorAlert="errorAlert" :errorMessage="errorMessage" />
@@ -42,6 +27,8 @@ import { computed, onMounted, reactive, ref } from "vue";
 import { useRoute } from "vue-router";
 import SuccessMessage from "@/components/SuccessMessage.vue";
 import ErrorMessage from "@/components/ErrorMessage.vue";
+import Button from "@/components/Inputs/Button.vue";
+import InputField from "@/components/Inputs/InputField.vue";
 
 onMounted(() => {
   initTooltips();
@@ -71,17 +58,19 @@ const handleImageChange = async (event: any) => {
 
     try {
       uploading.value = true;
+      showSuccessMessage.value = true;
       const response = await axios.post(`${url}/upload`, formData);
       slider.image = response.data;
       uploading.value = false;
       setTimeout(() => {
-        showSuccessMessage.value = true;
+        showSuccessMessage.value = false;
       }, 1000)
       successMessage.value = isEditing.value ? 'New image uploaded successfully!' : 'Image uploaded successfully!';
     } catch (error: any) {
       uploading.value = false;
+      errorAlert.value = true;
       setTimeout(() => {
-        errorAlert.value = true;
+        errorAlert.value = false;
       }, 1500)
       errorMessage.value = error?.message;
     }
@@ -117,7 +106,7 @@ const handleAddSlider = async () => {
     setTimeout(() => {
       showSuccessMessage.value = true;
     }, 1000)
-    successMessage.value = isEditing.value ? 'Post updated successfully!' : 'Post created successfully!';
+    successMessage.value = isEditing.value ? 'Slider updated successfully!' : 'Slider added successfully!';
 
     setTimeout(() => {
       showSuccessMessage.value = false;
@@ -137,11 +126,5 @@ const handleAddSlider = async () => {
     errorMessage.value = error.message;
   }
 };
-
 </script>
-<style>
-/* * {
-    outline: 1px solid;
-  } */
-</style>
   
