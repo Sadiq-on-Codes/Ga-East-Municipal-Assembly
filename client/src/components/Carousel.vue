14962a4ff2e95a8ef3637ec4df197850738166a0
Sadick Abubakar Yahaya <52420004+SYahaya-Abubakar@users.noreply.github.com>
2024-03-19 16:59:14 +0000

    carousel fix

--- client/src/components/Carousel.vue
+++ client/src/components/Carousel.vue
@@ -1,44 +1,35 @@
 <template>
   <div id="animation-carousel" class="relative mb-34" data-carousel-interval="15000" data-carousel="slide">
     <!-- Carousel wrapper -->
+
     <div class="relative overflow-hidden md:h-screen h-56">
       <!-- Item 1 -->
-      <div v-for="(image, i) in allSliders" :key="i" class="hidden duration-700 ease-in-out" data-carousel-item="active">
-        <div>
-          <img :src="`${imagesUrl}/uploads/${image?.image}`"
-            class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-        </div>
+      <Loader v-if="loading" />
+      <div v-else>
+        <img v-if="activeImage" :src="`${imagesUrl}/uploads/${activeImage?.image}`" :data-carousel-item="activeIndex"
+          class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
+
         <div class="overlay md:h-screen h-52 absolute inset-0 bg-gray-900 bg-opacity-60"></div>
-        <div class="carousel-description absolute inset-0 flex flex-col gap-10 items-start justify-center w-4/5">
+        <div class="carousel-description absolute inset-0 flex flex-col gap-10 items-start justify-center w-4/5"
+          v-if="activeImage">
           <div class="flex flex-col gap-6">
-            <h1 class="text-6xl uppercase text-left font-semibold text-white w-7/12">
-              {{ image?.title }}
-            </h1>
-            <span class="text-left font-news-heading text-white w-6/12">{{
-              image?.description
-            }}</span>
+            <h1 class="text-6xl uppercase text-left font-semibold text-white w-7/12">{{ activeImage?.title }}</h1>
+            <span class="text-left font-news-heading text-white w-6/12">{{ activeImage?.description }}</span>
           </div>
         </div>
       </div>
     </div>
     <!-- Slider indicators -->
-    <div
-      class="absolute z-30 flex dark:text-white space-x-3 -translate-x-1/2 bottom-5 left-1/2"
-    >
-      <button
-        v-for="(item, index) in allSliders"
-        :key="index"
-        type="button"
-        class="w-3 h-3 rounded-full"
-        aria-current="false"
-        :aria-label="index.toString()"
-        :data-carousel-slide-to="item.count"
-      ></button>
+    <div class="absolute z-30 flex dark:text-white space-x-3 -translate-x-1/2 bottom-5 left-1/2">
+      <button v-for="(item, index) in allSliders" :key="index" type="button" class="w-3 h-3 rounded-full"
+        :class="{ 'bg-white': index === activeIndex }" :aria-current="index === activeIndex ? 'true' : 'false'"
+        :aria-label="index.toString()" @click="setActiveIndex(index)" :data-carousel-slide-to="index"></button>
     </div>
+    <!-- Slider controls -->
     <!-- Slider controls -->
     <button type="button"
       class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-      data-carousel-prev>
+      data-carousel-prev @click="prevImage">
       <span
         class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-white group-hover:bg-white/50 dark:group-hover:bg-button-bg group-focus:ring-4 group-focus:ring-gray-800/60 dark:group-focus:ring-gray-800/70 group-focus:outline-none">
         <svg aria-hidden="true" class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800 dark:hover:text-white"
@@ -50,7 +41,7 @@
     </button>
     <button type="button"
       class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-      data-carousel-next>
+      data-carousel-next @click="nextImage">
       <span
         class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-white group-hover:bg-white/50 dark:group-hover:bg-button-bg group-focus:ring-4 group-focus:ring-gray-800/60 dark:group-focus:ring-gray-800/70 group-focus:outline-none">
         <svg aria-hidden="true" class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800 dark:hover:text-white"
@@ -60,6 +51,7 @@
         <span class="sr-only">Next</span>
       </span>
     </button>
+
     <span @click="sendEmail()" data-tooltip-target="tooltip-light" data-tooltip-style="light"
       class="email cursor-pointer carousel-description border-b-4 border-button-bg-hover font-bold text-xl text-white pb-2 absolute top-1/2 right-52 flex flex-col gap-10 items-end content-end justify-center h-fit w-fit">info@gema.gov.gh</span>
     <div id="tooltip-light" role="tooltip"
@@ -69,33 +61,22 @@
     </div>
   </div>
 </template>
+
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
+import { onMounted, ref, computed } from "vue";
 import { initCarousels, initTooltips, initPopovers } from "flowbite";
 import { url, imagesUrl } from "@/functions/endpoint";
 import axios from "axios";
-
-onMounted(() => {
-  console.log("Component is mounted.");
-  initTooltips();
-  initPopovers();
-  fetchSliders();
-  if (allSliders.value.length > 0) {
-    initCarousels();
-    console.log("Carousel initialized.");
-  }
-});
-
+import Loader from "./Loader.vue";
 
 const loading = ref(false);
 const email = ref("info@gema.gov.gh");
 const subject = ref("Ga East Municipal Assembly, Head Office");
 const body = ref("Send us an email and we will get back to you");
-const sendEmail = () => {
-  window.location.href = `mailto:${email.value}?subject=${subject.value}&body=${body.value}`;
-};
 
-const allSliders: any = ref([]);
+const allSliders = ref<any>([]);
+const activeIndex = ref(0);
+
 const fetchSliders = () => {
   loading.value = true;
   axios
@@ -108,6 +89,11 @@ const fetchSliders = () => {
       }));
       allSliders.value = slidersWithKey;
       console.log(allSliders.value);
+      initTooltips(); // Initialize tooltips after fetching sliders
+      if (allSliders.value.length > 0) {
+        initCarousels();
+        console.log("Carousel initialized.");
+      }
     })
     .catch((error: string) => {
       console.log(error);
@@ -116,7 +102,32 @@ const fetchSliders = () => {
       loading.value = false;
     });
 };
+
+onMounted(fetchSliders);
+
+const sendEmail = () => {
+  const encodedSubject = encodeURIComponent(subject.value);
+  const encodedBody = encodeURIComponent(body.value);
+  window.location.href = `mailto:${email.value}?subject=${encodedSubject}&body=${encodedBody}`;
+};
+
+const activeImage = computed(() => {
+  return allSliders.value[activeIndex.value];
+});
+
+const setActiveIndex = (index: number) => {
+  activeIndex.value = index;
+};
+
+const nextImage = () => {
+  activeIndex.value = (activeIndex.value + 1) % allSliders.value.length;
+};
+
+const prevImage = () => {
+  activeIndex.value = (activeIndex.value - 1 + allSliders.value.length) % allSliders.value.length;
+};
 </script>
+
 <style>
 .animate-fade {
   animation: fade 2s ease-in-out infinite;
@@ -138,5 +149,4 @@ const fetchSliders = () => {
 
 .email {
   z-index: 100;
-}
-</style>
+}</style>
